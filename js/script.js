c80bdec19afa64910deb4740ce7188bea8edd8f4
mpinzon <mspinzon406@misena.edu.co>
2022-08-04 08:41:41 -0500

    Variables y Console.log básico JavaScript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
 //Estructura basica - Es decir , la consola.
 console.log("Hola Mundo");
 
+//Dato almacenado - Variable almacenada
+let number=25;
+//Primera forma de trabajo
+console.log("El numero es ",number);
+//Segunda forma - Es mas usual verla
+console.log(`El numero es ${number}`);
+
+////////////////////////////////////////////
+
+//Dato almacenado - Variable almacenada
+let name="Michael";
+//Ver contenido con la segunda forma
+console.log(`El nombre es ${name}`);
+
+/////////////////////////////////////////////
+
+//Dato almacenado - Variables almacenadas , una definida y otra no
+let datoboolean= new Boolean(true);
+let datobooleanvacia= new Boolean();
+//Ver contenido
+console.log(`El dato es ${datoboolean}`);
+console.log(`El dato es ${datobooleanvacia}`);
+
+/////////////////////////////////////////////
+
+//Dato almacenado - Variable almacenada
+let res=number+5
+//Ver contenido 
+console.log(`El resultado es ${number+5}`); //O tambien ${res} , que es lo más recomendado. 
+
+/////////////////////////////////////////////
